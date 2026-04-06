f4ceebe6e0da4581730ba6b60486d87bfea51b6f
SHUBHAM-NAGDIYA <143119328+SHUBHAM-NAGDIYA@users.noreply.github.com>
2026-04-07 00:32:18 +0530

    Change API base URL to production server

--- WorkNest_UI/src/services/api.js
+++ WorkNest_UI/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://worknest-elmw.onrender.com",
   withCredentials: true
 });
 
